09404e79243175187db23ac44456fa09afdd38ee
iolyab <iolyabru@gmail.com>
2025-08-14 11:34:21 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import classes from "./App.module.scss";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Star Wars Characters</h1>
+      </header>
+      <main>
+        <p>This will be the people list</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
